799c3716f8a1a0c012b59b828b83a549a9f8150b
Todd Hancock <thancock20@hotmail.com>
2016-11-02 23:28:43 -0500

    Add routes

--- client/modules/core/routes.jsx
+++ client/modules/core/routes.jsx
@@ -3,6 +3,9 @@ import {mount} from 'react-mounter';
 
 import MainLayout from './components/main_layout.jsx';
 import Home from './components/home.jsx';
+import PollListScreen from './components/poll_list_screen.jsx';
+import PollScreen from './components/poll_screen.jsx';
+import NewPollForm from './components/new_poll_form.jsx';
 
 export default function (injectDeps, {FlowRouter}) {
   const MainLayoutCtx = injectDeps(MainLayout);
@@ -12,6 +15,33 @@ export default function (injectDeps, {FlowRouter}) {
     action() {
       mount(MainLayoutCtx, {
         content: () => (<Home />)
+      });
+    }
+  });
+
+  FlowRouter.route('/polls', {
+    name: 'polls',
+    action() {
+      mount(MainLayoutCtx, {
+        content: () => (<PollListScreen />)
+      });
+    }
+  });
+
+  FlowRouter.route('/polls/new', {
+    name: 'newPoll',
+    action() {
+      mount(MainLayoutCtx, {
+        content: () => (<NewPollForm />)
+      });
+    }
+  });
+
+  FlowRouter.route('/polls/:id', {
+    name: 'poll',
+    action() {
+      mount(MainLayoutCtx, {
+        content: () => (<PollScreen />)
       });
     }
   });
