641889e8a58c7e6a491d2ba6baa4d746e2ba7db8
Todd Hancock <thancock20@hotmail.com>
2016-11-29 15:15:20 -0600

    Add tests for calling actions

--- client/modules/core/components/tests/poll.js
+++ client/modules/core/components/tests/poll.js
@@ -1,6 +1,7 @@
 const {describe, it} = global;
 import {expect} from 'chai';
 import {shallow} from 'enzyme';
+import {spy} from 'sinon';
 import Poll from '../poll';
 
 describe('core.components.poll', () => {
@@ -11,5 +12,22 @@ describe('core.components.poll', () => {
       isOwner: () => true
     };
     expect(shallow(<Poll poll={poll} deletePoll={() => {}} />).length).to.equal(1);
+  });
+
+  it('should call deletePoll when button clicked', () => {
+    const deletePoll = spy();
+    const poll = {
+      _id: 'abc123',
+      question: 'Ice Cream Flavors',
+      isOwner: () => true
+    };
+    const el = shallow(<Poll poll={poll} deletePoll={deletePoll} />);
+
+    el.find('ButtonCircle').first().simulate('click');
+    const args = deletePoll.args[0];
+
+    expect(args.slice(0,1)).to.deep.equal([
+      'abc123'
+    ]);
   });
 });
--- client/modules/core/components/tests/poll_list_screen.js
+++ client/modules/core/components/tests/poll_list_screen.js
@@ -1,10 +1,20 @@
 const {describe, it} = global;
 import {expect} from 'chai';
 import {shallow} from 'enzyme';
+import { spy } from 'sinon';
 import PollListScreen from '../poll_list_screen';
 
 describe('core.components.poll_list_screen', () => {
   it('should render without exploding', () => {
     expect(shallow(<PollListScreen newPoll={() => {}} />).length).to.equal(1);
+  });
+
+  it('should call newPoll when button clicked', () => {
+    const newPoll = spy();
+    const el = shallow(<PollListScreen newPoll={newPoll} />);
+
+    el.find('Button').simulate('click');
+
+    expect(newPoll.called).to.equal(true);
   });
 });
