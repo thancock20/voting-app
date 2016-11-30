0d296fe46b0a06bc67048c83d7692ca541131a25
Todd Hancock <thancock20@hotmail.com>
2016-11-29 18:00:25 -0600

    Build PollChart component

--- client/modules/core/components/.stories/poll_chart.js
+++ client/modules/core/components/.stories/poll_chart.js
@@ -3,11 +3,27 @@ import { storiesOf, action } from '@kadira/storybook';
 import { withKnobs, text, boolean, number, object } from '@kadira/storybook-addon-knobs';
 import { setComposerStub } from 'react-komposer';
 import PollChart from '../poll_chart.jsx';
+import palette from '../../libs/palette';
 
 storiesOf('core.PollChart', module)
   .addDecorator(withKnobs)
   .add('default view', () => {
+    const options = object('options', [
+      {
+        name: 'Chocolate',
+        votes: 10
+      },
+      {
+        name: 'Vanilla',
+        votes: 5
+      },
+      {
+        name: 'Rocky Road',
+        votes: 20
+      }
+    ]);
+    const colorScale = palette([ 'tol', 'tol-rainbow' ], options.length);
     return (
-      <PollChart />
+      <PollChart options={options} colorScale={colorScale} />
     );
   });
--- client/modules/core/components/poll_chart.jsx
+++ client/modules/core/components/poll_chart.jsx
@@ -1,10 +1,19 @@
 import React, { PropTypes } from 'react';
+import { VictoryPie } from 'victory';
 
-const PollChart = () => (
-  <div>
-    PollChart
-  </div>
+const PollChart = ({options, colorScale}) => (
+  <VictoryPie
+    data={options}
+    x='name'
+    y='votes'
+    colorScale={colorScale}
+    innerRadius={100}
+    labels={() => {}}
+  />
 );
-PollChart.propTypes = {};
+PollChart.propTypes = {
+  options: PropTypes.array,
+  colorScale: PropTypes.array
+};
 
 export default PollChart;
