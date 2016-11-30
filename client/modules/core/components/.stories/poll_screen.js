a2a81969fbbcbf34f50e7932c09f517a0c228877
Todd Hancock <thancock20@hotmail.com>
2016-11-29 21:08:59 -0600

    Build PollScreen component

--- client/modules/core/components/.stories/poll_screen.js
+++ client/modules/core/components/.stories/poll_screen.js
@@ -7,7 +7,25 @@ import PollScreen from '../poll_screen.jsx';
 storiesOf('core.PollScreen', module)
   .addDecorator(withKnobs)
   .add('default view', () => {
+    const poll = object('poll', {
+      _id: 'abc123',
+      question: 'Ice Cream Flavors',
+      options: [
+        {
+          name: 'Chocolate',
+          votes: 10
+        },
+        {
+          name: 'Vanilla',
+          votes: 5
+        },
+        {
+          name: 'Rocky Road',
+          votes: 20
+        }
+      ]
+    });
     return (
-      <PollScreen />
+      <PollScreen poll={poll} />
     );
   });
--- client/modules/core/components/poll_screen.jsx
+++ client/modules/core/components/poll_screen.jsx
@@ -1,14 +1,29 @@
 import React, { PropTypes } from 'react';
 import PollChart from './poll_chart.jsx';
 import PollButtons from './poll_buttons.jsx';
+import palette from '../libs/palette';
+import { Heading } from 'rebass';
 
-const PollScreen = () => (
-  <div style={{paddingTop: 48}}>
-    PollScreen
-    <PollChart />
-    <PollButtons />
-  </div>
-);
+const PollScreen = ({poll}) => {
+  const colorScale = palette([ 'tol', 'tol-rainbow' ], poll.options.length);
+  return (
+    <div style={{paddingTop: 48}}>
+      <Heading
+        level={1}
+        style={{textAlign: 'center'}}
+      >
+        {poll.question}
+      </Heading>
+      <PollChart
+        options={poll.options}
+        colorScale={colorScale}
+      />
+      <PollButtons
+        poll={poll}
+      />
+    </div>
+  );
+};
 PollScreen.propTypes = {};
 
 export default PollScreen;
