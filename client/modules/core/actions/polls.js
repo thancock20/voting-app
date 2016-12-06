fd97e573f01164b239edf962bb9ea0ac0fb67347
Todd Hancock <thancock20@hotmail.com>
2016-12-06 17:11:23 -0600

    Add vote action

--- client/modules/core/actions/polls.js
+++ client/modules/core/actions/polls.js
@@ -1,5 +1,17 @@
 export default {
-  vote() {},
+  vote({Meteor, Collections}, pollId, optionName) {
+    Meteor.subscribe('polls');
+    const poll = Collections.Polls.findOne({_id: pollId});
+    const options = poll.options.map((option) => {
+      if (option.name === optionName) {
+        option.votes++;
+      }
+      return option;
+    });
+
+    Meteor.call('polls.edit', pollId, {options});
+  },
+
   addOption() {},
 
   deletePoll({Meteor}, pollId) {
--- client/modules/core/actions/tests/polls.js
+++ client/modules/core/actions/tests/polls.js
@@ -4,6 +4,56 @@ import {spy, stub} from 'sinon';
 import actions from '../polls';
 
 describe('core.actions.polls', () => {
+  describe('vote', () => {
+    it('should call Meteor.call to add a vote to the option', () => {
+      const Meteor = {call: spy(), subscribe: stub()};
+      Meteor.subscribe.returns({ready: () => false});
+
+      const poll = {
+        _id: 'abc123',
+        question: 'Ice Cream Flavor',
+        options: [
+          {
+            name: 'Chocolate',
+            votes: 0
+          },
+          {
+            name: 'Vanilla',
+            votes: 0
+          },
+          {
+            name: 'Rocky Road',
+            votes: 0
+          }
+        ]
+      };
+      const Collections = {Polls: {findOne: stub()}};
+      Collections.Polls.findOne.returns(poll);
+
+      actions.vote({Meteor, Collections}, 'abc123', 'Chocolate');
+      const args = Meteor.call.args[0];
+
+      expect(args.slice(0, 3)).to.deep.equal([
+        'polls.edit',
+        'abc123',
+        {options: [
+          {
+            name: 'Chocolate',
+            votes: 1
+          },
+          {
+            name: 'Vanilla',
+            votes: 0
+          },
+          {
+            name: 'Rocky Road',
+            votes: 0
+          }
+        ]}
+      ]);
+    });
+  });
+
   describe('deletePoll', () => {
     it('should call Meteor.call to delete the poll', () => {
       const Meteor = {call: spy()};
