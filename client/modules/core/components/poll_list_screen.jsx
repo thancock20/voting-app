c4e33d6b6426dba0961c13350cb0f1bd69d8753c
Todd Hancock <thancock20@hotmail.com>
2016-12-06 15:13:45 -0600

    Remove New Poll button from PollListScreen

--- client/modules/core/components/poll_list_screen.jsx
+++ client/modules/core/components/poll_list_screen.jsx
@@ -1,17 +1,9 @@
 import React, { PropTypes } from 'react';
 import PollList from '../containers/poll_list.js';
-import { Button } from 'rebass';
 
 const PollListScreen = ({newPoll}) => (
   <div style={{paddingTop: 48}}>
     <PollList />
-    <Button
-      pill={true}
-      theme='primary'
-      onClick={newPoll}
-    >
-      New Poll
-    </Button>
   </div>
 );
 PollListScreen.propTypes = {
