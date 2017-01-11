fd9117d02d2e4ec6ad43f9f3b4d09335251642e1
Todd Hancock <thancock20@hotmail.com>
2017-01-10 23:28:40 -0600

    Fix routes.jsx in accounts module

--- client/modules/accounts/routes.jsx
+++ client/modules/accounts/routes.jsx
@@ -1,17 +1,24 @@
-// import React from 'react';
-// import {mount} from 'react-mounter';
-//
-// import MainLayout from '/client/modules/core/components/main_layout.jsx';
-//
-// export default function (injectDeps, {FlowRouter}) {
-//   const MainLayoutCtx = injectDeps(MainLayout);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
-  // FlowRouter.route('', {
-  //   name: '',
-  //   action() {
-  //     mount(MainLayoutCtx, {
-  //       content: () => (< />)
-  //     });
-  //   }
+import MainLayout from '/client/modules/core/components/main_layout.jsx';
+
+export default function (injectDeps, {Meteor}) {
+  const MainLayoutCtx = injectDeps(MainLayout);
+
+  // Meteor.startup(() => {
+  //   ReactDOM.render(
+  //     (<Router
+  //       history={browserHistory}
+  //       onUpdate={() => {window.scrollTo(0, 0);}}>
+  //       <Route
+  //         path=""
+  //         component = {MainLayoutCtx}>
+  //         <IndexRoute component={} />
+  //       </Route>
+  //     </Router>)
+  //   );
   // });
-// }
+
+}
